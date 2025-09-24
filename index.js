378b60c409ac639fdaa6967fa18c8fea364bf36e
karthikeya-tummala <karthikeyatummala@gmail.com>
2025-09-24 12:59:03 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import "express-async-error";
+import dotenv from "dotenv";
+import { pool } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: err.message });
+});
+
+// Test DB connection on startup
+try {
+    const res = await pool.query('SELECT NOW()');
+    console.log('Postgres is up:', res.rows[0]);
+} catch (err) {
+    console.error('Postgres connection error:', err);
+}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
